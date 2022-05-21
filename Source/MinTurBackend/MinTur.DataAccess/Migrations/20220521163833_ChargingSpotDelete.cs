ac3cc2153ac51e35497ff3750c93f5a67c5b6bda
Joaquin Meerhoff <jm247096@fi365.ort.edu.uy>
2022-05-21 15:12:24 -0300

    fix(charging-spot-migration): adds migration for removing charging spot
    Includes fix for webapi controller and connection string for migrations that was hardcoded in context

---
+++ Source/MinTurBackend/MinTur.DataAccess/Migrations/20220521163833_ChargingSpotDelete.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MinTur.DataAccess.Migrations
+{
+    public partial class ChargingSpotDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChargingSpots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    RegionId = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargingSpots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChargingSpots_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargingSpots_RegionId",
+                table: "ChargingSpots",
+                column: "RegionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChargingSpots");
+        }
+    }
+}
