f3b1b8bbae3b11c488a79bbf1c9bd48372c87a25
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-05-21 12:14:25 -0300

    feat(charging spot data access): implements delete charging spot by id with tdd
    Implements delet charging spot by id with tdd and adds method to context and facade

---
+++ Source/MinTurBackend/MinTur.DataAccess.Test/Repositories/ChargingSpotRepositoryTest.cs
@@ -0,0 +1,64 @@
+﻿using MinTur.DataAccess.Contexts;
+using MinTur.DataAccess.Repositories;
+using MinTur.Domain.BusinessEntities;
+using MinTur.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinTur.DataAccess.Test.Repositories
+{
+    [TestClass]
+    public class ChargingSpotRepositoryTest
+    {
+        private ChargingSpotRepository _repository;
+        private NaturalUruguayContext _context;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _context = ContextFactory.GetNewContext(ContextType.Memory);
+            _repository = new ChargingSpotRepository(_context);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _context.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void DeleteChargingSpotReturnsAsExpected()
+        {
+            ChargingSpot chargingSpotToBeDeleted = CreateChargingSpot();
+            _context.Set<Region>().Add(chargingSpotToBeDeleted.Region);
+            _context.Set<ChargingSpot>().Add(chargingSpotToBeDeleted);
+            _context.SaveChanges();
+
+            _repository.DeleteChargingSpotById(chargingSpotToBeDeleted.Id);
+            Region regionInDb = _context.Set<Region>().ToList()[0];
+            List<ChargingSpot> chargingSpotsInDB = _context.Set<ChargingSpot>().ToList();
+            Assert.AreEqual(0, chargingSpotsInDB.Count);
+            Assert.AreEqual(chargingSpotToBeDeleted.Region, regionInDb);
+        }
+
+        #region Helpers
+        private ChargingSpot CreateChargingSpot()
+        {
+            return new ChargingSpot()
+            {
+                Address = "Direccion",
+                Description = "Descripcion ....",
+                Name = "Hotel Italiano carga",
+                Region = new Region()
+                {
+                    Name = "SurOeste"
+                }
+            };
+        }
+
+        #endregion
+    }
+}
--- Source/MinTurBackend/MinTur.DataAccess/Repositories/ChargingSpotRepository.cs
+++ Source/MinTurBackend/MinTur.DataAccess/Repositories/ChargingSpotRepository.cs
@@ -1,13 +1,25 @@
+using Microsoft.EntityFrameworkCore;
 using MinTur.DataAccessInterface.Repositories;
+using MinTur.Domain.BusinessEntities;
 using System;
+using System.Linq;
 
 namespace MinTur.DataAccess.Repositories
 {
     public class ChargingSpotRepository : IChargingSpotRepository
     {
+        protected DbContext Context { get; set; }
+
+        public ChargingSpotRepository(DbContext dbContext)
+        {
+            Context = dbContext;
+        }
+
         public void DeleteChargingSpotById(int chargingSpotId)
         {
-            throw new NotImplementedException();
+            ChargingSpot chargingSpotFromDb = Context.Set<ChargingSpot>().FirstOrDefault(c => c.Id == chargingSpotId);
+            Context.Set<ChargingSpot>().Remove(chargingSpotFromDb);
+            Context.SaveChanges();
         }
     }
 }
